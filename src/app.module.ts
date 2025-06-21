26f6f487c19b8e38d41f6ff51e9718abcf719ff7
Juan Prieto <jp1739@gmail.com>
2025-06-21 09:52:42 -0500

    feat: install dependencies and import TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'tu_usuario_mysql',
+      password: 'tu_password',
+      database: 'burger_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
