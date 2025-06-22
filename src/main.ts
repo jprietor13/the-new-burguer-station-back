05ec5bb0c23378f4c9b0e8f297ee6da797e4b8e9
Juan Prieto <jp1739@gmail.com>
2025-06-22 11:14:37 -0500

    fix: configure crypyo

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+if (!globalThis.crypto) {
+  (globalThis as any).crypto = require('crypto');
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('The new burger station api')
-    .setDescription(
-      'API para la gestion de usuarios, ordenes y menu de hamburguesas',
-    )
+  const config = new DocumentBuilder()
+    .setTitle('Burger Station API')
+    .setDescription('Documentación de la API')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
